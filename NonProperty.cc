3666d5c31006c981c67f312cbd9b4b5aa34504a6
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-05 17:02:44 -0400

    Update NonProperty.cc

--- NonProperty.cc
+++ NonProperty.cc
@@ -2,14 +2,48 @@
 
 Tuition::~Tuition() {}
 
-Tuition::accept(Player p) {
+Tuition::accept(Player & p) {
   throw WatopolyException("Tuition");
 }
-
-TimHortons::TimHortons() {}
 
 TimHortons::~TimHortons() {}
 
 TimHortons::accept(Player & p) {
-  
+  //to be added
+}
+
+GoToTims::~GoToTims() {}
+
+GoToTims::accept(Player & p) {
+  throw WatopolyException("Move");
+}
+
+SLC::~SLC(){}
+
+SLC::accept(Player & p) {
+  throw WatopolyException("SLC");
+}
+
+OSAP::~OSAP(){}
+
+OSAP::accept(Player & p) {
+  p->addMoney(200);
+}
+
+CoopFee::~CoopFee(){}
+
+CoopFee::accept(Player & p) {
+  p->giveMoney(nullptr, 150);
+}
+
+Goose::~Goose(){}
+
+Goose::accept(Player & p) {
+  throw WatopolyException("Attacked by geese!");
+}
+
+NeedlesHall::~NeedlesHall(){}
+
+NeedlesHall::accept(Player & p) {
+  //to be added, check the probability and change the amount of money
 }
