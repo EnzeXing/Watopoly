cbc13122fdeb8a9c0f9feb2d8e4ec9d3a292786c
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-05 16:03:53 -0400

    Update NonProperty.cc

--- NonProperty.cc
+++ NonProperty.cc
@@ -3,5 +3,13 @@
 Tuition::~Tuition() {}
 
 Tuition::accept(Player p) {
+  throw WatopolyException("Tuition");
+}
+
+TimHortons::TimHortons() {}
+
+TimHortons::~TimHortons() {}
+
+TimHortons::accept(Player & p) {
   
 }
