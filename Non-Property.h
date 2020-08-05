1b2ad975b0221a954913d4fb983cd039a979293f
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-05 14:45:45 -0400

    Update Non-Property.h

--- Non-Property.h
+++ Non-Property.h
@@ -1,6 +1,62 @@
+#ifndef NONPROPERTY_H
+#define NONPROPERTY_H
 #include "Building.h"
+#include <vector>
 
-class Non-Property : public Building {
+class NonProperty : public Building {
   public:
-    virtual ~Non-Property() = 0;
+    virtual ~NonProperty() = 0;
 };
+
+class Tuition : public NonProperty {
+  public:
+    virtual ~Tuition() override;
+    virtual accept(Player p) override;
+};
+
+class TimHortons : public NonProperty {
+    std::vector<std::shared_ptr<Player>> line;
+  public:
+    TimHortons();
+    virtual ~TimHortons() override;
+    virtual accept(Player p) override;
+};
+
+class GoToTims : public NonProperty {
+  public:
+    virtual ~GoToTims() override;
+    virtual accept(Player p) override;
+};
+
+class SLC : public NonProperty {
+  public:
+    virtual ~SLC() override;
+    virtual accpet(Player p) override;
+};
+
+class OSAP : public NonProperty {
+  public:
+    virtual ~OSAP() override;
+    virtual accept(Player p) override;
+    unsigned int giveMoney();
+};
+
+class CoopFee : public NonProperty {
+  public:
+    virtual ~CoopFee() override;
+    virtual accept(Player p) override;
+};
+
+class Goose : public NonProperty {
+  public:
+    virtual ~Goose() override;
+    virtual accept(Player p) override;
+};
+
+class NeedlesHall : public NonProperty {
+  public:
+    virtual ~NeedlesHall() override;
+    virtual accept(Player p) override;
+};
+
+#endif
