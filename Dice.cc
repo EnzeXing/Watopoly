9a0f8c9f6fc3a088df128b80b52b434268154cd0
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-05 17:30:14 -0400

    Update Dice.cc

--- Dice.cc
+++ Dice.cc
@@ -2,8 +2,9 @@
 
 
 int Dice::roll() {
-  int a;
-  strand (time(NULL));
-  a = rand() % 6 + 1;
-  return a;
+        std::random_device r;
+        std::default_random_engine e1(r());
+        std::uniform_int_distribution<int> uniform_dist(1, 6);
+        int a = uniform_dist(e1);
+        return a;
 }
