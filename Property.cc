bf378b676ef1dab94edfa5edabfe10c6445afb1a
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-05 15:06:39 -0400

    Update Property.cc

--- Property.cc
+++ Property.cc
@@ -5,6 +5,23 @@ std::string Property::getOwner() {
     return Owner;
 }
 
-unsigned int Property::getFee() {
-    
+unsigned int Gym::getFee() {
+}
+
+void Gym::accept(Player & p) {
+    unsigned int fee = getFee();
+    p.addMoney(fee);
+}
+
+unsigned int Residence::getPurchaseCost() {
+    return purchaseCost;
+}
+
+unsigned int Resisdence::getRent() {
+}
+
+void Residence::accept(Player p) {
+    unsigned int rent = getRent();
+    p.addMoney(rent);
+}
 
