068a8d8720c4e571e0d193fc5f1184a2927687e9
EnzeXing <58994529+EnzeXing@users.noreply.github.com>
2020-08-05 16:03:35 -0400

    Create Player.cc

---
+++ Player.cc
@@ -0,0 +1,74 @@
+#include "Player.h"
+#include <stdexcept>
+
+std::string Player::getName() {
+    return name;
+}
+
+char Player::getSymbol() {
+    return symbol;
+}
+
+int Player::getMoney() {
+    return money;
+}
+
+void Player::giveMoney(std::shared_ptr<Player> other, int amount) {
+    if (money < amount) {
+        throw WatopolyException("Not enough money!");
+    }
+    
+    money -= amount;
+    if (other != nullptr) other->addMoney(amount);
+}
+
+void Player::addMoney(int amount) {
+    money += amount;
+}
+
+unsigned int Player::getProperties(std::string type) {
+    return properties[type];
+}
+
+void Player::addBuilding(std::string type) {
+    properties[type]++;
+}
+
+void Player::loseBuilding(std::string type) {
+    properties[type]--;
+}
+
+bool Player::hasFullMonopoly(std::string type) {
+    if (type == "Arts1") {
+        return properties[type] == 2;
+    } else if (type == "Arts2") {
+        return properties[type] == 3;
+    } else if (type == "Eng") {
+        return properties[type] == 3;
+    } else if (type == "Health") {
+        return properties[type] == 3;
+    } else if (type == "Env") {
+        return properties[type] == 3;
+    } else if (type == "Sci1") {
+        return properties[type] == 3;
+    } else if (type == "Sci2") {
+        return properties[type] == 3;
+    } else if (type == "Math") {
+        return properties[type] == 2;
+    } else if (type == "Residence") {
+        return properties[type] == 4;
+    } else {
+        return properties[type] == 2;
+    }
+}
+
+void Player::addCup() {
+    rimCup++;
+}
+
+void Player::removeCup() {
+    if (rimCup == 0) {
+        throw WatopolyException("Not enough Rim Cup!");
+    }
+    rimCup--;
+}
