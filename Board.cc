d3a726b4d77a6912963b6fbe3de38c9e80fcd3c9
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-06 15:33:04 -0400

    Update Board.cc

--- Board.cc
+++ Board.cc
@@ -1,1 +1,11 @@
+#include "Board.h"
 
+class Board {
+  std::vector<40, std::vector<7, std::string>> board;
+  
+  public:
+  Board(vector<shared_ptr<40, Buildings>> b);
+};
+
+Board::Board(vector<shared_ptr<40, Buildings>> b) {
+  vector<40, >
