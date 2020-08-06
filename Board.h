2b87980c7fed90419fde89acc23b42088bac784b
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-06 15:30:25 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,1 +1,11 @@
+#include <string>
+#include <memory>
+#include <vector>
+
+class Board {
+  std::vector<40, std::vector<7, std::string>> board;
+  
+  public:
+  Board(vector<shared_ptr<40, Buildings>> b);
+};
 
