9b90b6f1fac41593475bd7687717a30b9b2c4731
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-06 15:34:39 -0400

    Update Board.cc

--- Board.cc
+++ Board.cc
@@ -4,8 +4,8 @@ class Board {
   std::vector<40, std::vector<7, std::string>> board;
   
   public:
-  Board(vector<shared_ptr<40, Buildings>> b);
+  Board(std::vector<std::shared_ptr<40, Buildings>> b);
 };
 
-Board::Board(vector<shared_ptr<40, Buildings>> b) {
-  vector<40, >
+Board::Board(std::vector<std::shared_ptr<40, Buildings>> b) {
+  vector<40, std::vector<7, std::string>>
