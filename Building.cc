ca5347221de38efbd175df5ccec3973049a85c15
EnzeXing <58994529+EnzeXing@users.noreply.github.com>
2020-08-06 17:19:47 -0400

    Update Building.cc

--- Building.cc
+++ Building.cc
@@ -1,6 +1,8 @@
 #include "Building.h"
 #include "Player.h"
 
+Building::Building(std::string name, int position) : name{name}, position{position} {}
+
 std::string Building::getName() {
     return name;
 }
