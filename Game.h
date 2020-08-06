2eb33f50fe8866807c8901c5ee0f411336e27442
EnzeXing <58994529+EnzeXing@users.noreply.github.com>
2020-08-06 15:34:46 -0400

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,17 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include "Player.h"
+#include "Building.h"
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <algorithm>
+
+class Game {
+  private:
+    Game(std::map{})
+};
+
+#endif
