de848acbf9b11290bd21bcd9ab96b43d99e4d75c
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-06 18:01:25 -0400

    Update NonProperty.h

--- NonProperty.h
+++ NonProperty.h
@@ -11,48 +11,56 @@ class NonProperty : public Building {
 
 class Tuition : public NonProperty {
   public:
+    Tuition(std::string name, int position);
     virtual ~Tuition() override;
     virtual accept(Player & p) override;
 };
 
 class TimHortons : public NonProperty {
   public:
+    TimHortons(std::string name, int position);
     virtual ~TimHortons() override;
     virtual accept(Player & p) override;
 };
 
 class GoToTims : public NonProperty {
   public:
+    GoToTims(std::string name, int position);
     virtual ~GoToTims() override;
     virtual accept(Player & p) override;
 };
 
 class SLC : public NonProperty {
   public:
+    SLC(std::string name, int position);
     virtual ~SLC() override;
     virtual accept(Player & p) override;
 };
 
 class OSAP : public NonProperty {
   public:
+    OSAP(std::string name, int position);
     virtual ~OSAP() override;
     virtual accept(Player & p) override;
 };
 
 class CoopFee : public NonProperty {
   public:
+    CoopFee(std::string name, int position);
     virtual ~CoopFee() override;
     virtual accept(Player & p) override;
 };
 
 class Goose : public NonProperty {
   public:
+    Goose(std::string name, int position);
     virtual ~Goose() override;
     virtual accept(Player & p) override;
 };
 
 class NeedlesHall : public NonProperty {
   public:
+    NeedlesHall(std::string name, int position);
     virtual ~NeedlesHall() override;
     virtual accept(Player & p) override;
 };
