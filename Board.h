ee4a40d2ebd5800636d9238bc08230b9634f401f
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-06 15:35:01 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -6,6 +6,6 @@ class Board {
   std::vector<40, std::vector<7, std::string>> board;
   
   public:
-  Board(vector<shared_ptr<40, Buildings>> b);
+  Board(std::vector<std::shared_ptr<40, Buildings>> b);
 };
 
