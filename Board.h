ff6fa126939278466e23778925e37bac401694b1
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-06 20:47:52 -0400

    Add files via upload

---
+++ Board.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <memory>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <iomanip>
+
+class Board {
+  std::vector<std::vector<std::string>> board;
+
+  public:
+  Board();
+  void updateImprovement(int position, int improvementNumber);
+  void updatePlayer(int position, std::string playerName);
+  void drawBoard(std::ostream & out);
+};
