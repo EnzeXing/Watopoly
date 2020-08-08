99a112b11df975e2a48da5e39cd5dd29775ce67c
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-08 16:20:46 -0400

    Update Player.cc

--- Player.cc
+++ Player.cc
@@ -32,7 +32,7 @@ int Player::getMoney() {
 
 void Player::giveMoney(std::shared_ptr<Player> other, int amount) {
     if (money < amount) {
-        throw WatopolyException("Not enough money!");
+        //throw WatopolyException("Not enough money!");
     }
     
     money -= amount;
@@ -97,7 +97,7 @@ void Player::addCup() {
 
 void Player::removeCup() {
     if (rimCup == 0) {
-        throw WatopolyException("Not enough Rim Cup!");
+        //throw WatopolyException("Not enough Rim Cup!");
     }
     rimCup--;
 }
