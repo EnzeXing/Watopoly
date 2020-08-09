f504bae2857b87c09783a04382c6a771bda53c19
EnzeXing <58994529+EnzeXing@users.noreply.github.com>
2020-08-09 14:29:49 -0400

    Update Exception.h

--- Exception.h
+++ Exception.h
@@ -10,4 +10,22 @@ class NotEnoughMoney : public WatopolyException {
     int needAmount;
 };
 
-class NotEnoughCup : public WatopolyException
+class NotEnoughCup : public WatopolyException {};
+
+class TuitionException : public WatopolyException {};
+
+class TimHortonsException : public WatopolyException {};
+
+class getRimCup : public WatopolyException {};
+
+class SLCException : public WatopolyException {
+  public:
+    int steps;
+};
+
+class GooseException : public WatopolyException {};
+
+class NeedlesHallException : public WatopolyException {
+  public:
+    int amount;
+};
