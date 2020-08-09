b503855f174c65ca92a873a0bf46671ca6bf663d
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-09 14:45:46 -0400

    Update Exception.cc

--- Exception.cc
+++ Exception.cc
@@ -2,3 +2,9 @@
 
 WatopolyException::WatopolyExeption(std::string m) : message{m} {}
 
+NoOwner::NoOwner(std::string m) : WatopolyExeption(m) {}
+
+NotEnoughMoney::NotEnoughMoney(std::string m, int a) : WatopolyExeption(m), needAmount{a} {}
+
+
+
