bb74bd36fee9551ddf8a6d292a30a0dfae43c3ad
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-10 17:58:43 -0400

    Update commandInput.h

--- commandInput.h
+++ commandInput.h
@@ -4,9 +4,10 @@
 #include "game.h"
 #include <iostream>
 
-class commandInput {
+class CommandInput {
     std::shared_ptr<Game> game;
   public:
+    CommandInput(std::shared_ptr<Game> game);
     void readInput(std::istream in);
     void purchaseOrNot(std::istream in);
     void auction(std::istream in);
