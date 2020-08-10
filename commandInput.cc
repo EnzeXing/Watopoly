a652b0176e5b4d7d8e4c2df4dc1e607d95b4ed2c
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-10 19:35:19 -0400

    Update commandInput.cc

--- commandInput.cc
+++ commandInput.cc
@@ -7,10 +7,7 @@ void CommandInput::readInput(std::istream in) {
   while (!in.fail()) {
     in >> s;
     if (s == "roll") {
-      std::shared_ptr<Player> currPlayer = game->currPlayer();
-      int dice = currPlayer->roll();
-      dice += currPlayer->roll();
-      game->movePlayer(dice);
+      game->roll();
     } else if (s == "next") {
       game->nextPlayer();
     } else if (s == "trade") {
@@ -23,12 +20,25 @@ void CommandInput::readInput(std::istream in) {
       if (!in.fail()) {
         try {
           int giveMoney = std::stoi(give);
-          int getMoney = std::stoi(get);
-          std::shared_ptr<Board> board = game->getBoard();
-          board->printMessage("Cannot trade money for money!", std::cout);
-          continue;
-        } catch (std::invalid_argument) {
+          try {
+            int getMoney = std::stoi(get);
+            std::shared_ptr<Board> board = game->getBoard();
+            board->printMessage("Cannot trade money for money!", std::cout);
+            continue;
+          } catch (std::invalid_argument) {
+            
+          } catch (std::out_of_range) {
           
+          }
+        } catch (std::invalid_argument) {
+          try {
+            int getMoney = std::stoi(get);
+            
+          } catch (std::invalid_argument) {
+          
+          } catch (std::out_of_range) {
+          
+          }
         } catch (std::out_of_range) {
           
         }
