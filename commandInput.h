5f462cac3787f21a3409e64464c395e46bd04a00
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-11 16:50:40 -0400

    Update commandInput.h

--- commandInput.h
+++ commandInput.h
@@ -12,7 +12,7 @@ class CommandInput {
     void purchaseOrNot(std::istream & in, std::string building);
     void auction(std::istream & in, std::string building);
     void notEnoughMoney(std::istream & in, int amount);
-    void TimHortons(std::istream & in, shared_ptr<Player> p);
+    void TimHortons(std::istream & in, std::shared_ptr<Player> p);
 };
 
 #endif
