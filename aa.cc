f87a2c4fe7d87d7eca33639c056c8f55d4cfd07b
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-11 16:32:23 -0400

    Update aa.cc

--- aa.cc
+++ aa.cc
@@ -1,27 +1,21 @@
-void CommandInput::TimHortons(std::istream & in, std::shared_ptr<Player> p) {
+void CommandInput::TimHortons(std::istream & in) {
     game->printMessage("You are in the DC Tims Line :(\nDo you want to roll dice, pay $50 or use a Roll Up the Rim Cup?");
     std::string command;
     in >> command;
     if (command == "roll") {
-      int first = p->roll();
-      game->printMessage("Dice 1: " + std::to_string(first));
-      int second = p->roll();
-      game->printMessage("Dice 2: " + std::to_string(second));
-      if (first == second) {
+      if (game->rollDouble()) {
         game->printMessage("Your rolled doubles!\nYou are out of the line!");
-        p->resetTimRound();
+        game->leaveLine();
       } else {
         game->printMessage("Your didn't roll doubles...\nYou are still in the line ...");
-        p->addTimRound();
+        game->stayInLine();
       }
     } else if (command == "pay") {
-      p->giveMoney(nullptr, 50);
+      game->buyCoffee()
       game->printMessage("You payed $50!\nYou are out of the line!");
-      p->resetTimRound();
     } else if (command == "use") {
       game->useRimCup();
       game->printMessage("You used a Roll Up the Rim Cup!\nYou are out of the line!");
-      p->resetTimRound();
     }
 }
       
