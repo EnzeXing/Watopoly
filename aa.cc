3df0d9c4ba6a13840ca5074c6d04f2b8e5d8b910
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-11 15:45:57 -0400

    Update aa.cc

--- aa.cc
+++ aa.cc
@@ -1,1 +1,10 @@
-
+void CommandInput::TimHortons(std::istream & in, std::shared_ptr<Player> p) {
+  std::string command;
+  in >> command;
+  if (command == "roll") {
+    int first = p->roll();
+    int second = p->roll();
+    if (first == second) {
+      p
+    
+  
