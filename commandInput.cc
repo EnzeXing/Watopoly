5fe1d6243ba2c3b23203ed3b57bc6807d676a1a6
Hazzility <56942401+Hazzility@users.noreply.github.com>
2020-08-10 20:39:20 -0400

    Update commandInput.cc

--- commandInput.cc
+++ commandInput.cc
@@ -17,6 +17,10 @@ void CommandInput::readInput(std::istream in) {
       in >> name;
       in >> give;
       in >> get;
+      if (in.fail()) {
+        game->printMessage("Not enough arguments.");
+        break;
+      }
       if (!in.fail()) {
         try {
           int giveMoney = std::stoi(give);
@@ -43,16 +47,104 @@ void CommandInput::readInput(std::istream in) {
         } catch (std::invalid_argument) {
           try {
             int getMoney = std::stoi(get);
-            
+            game->printMessage("Does " + name + " accept the trade of giving $" + get + " in exchange for " + give + " ?");
+            std::string response;
+            std::cin >> response;
+            if (response == "accept") {
+              // do the trade
+            } else if (response == "reject") {
+              continue;
+            } else {
+              game->printMessage("Invalid response, trade cancelled.");
+              continue;
+            }
           } catch (std::invalid_argument) {
-          
+            game->printMessage("Does " + name + " accept the trade of giving " + get + " in exchange for " + give + " ?");
+            std::string response;
+            std::cin >> response;
+            if (response == "accept") {
+              // do the trade
+            } else if (response == "reject") {
+              continue;
+            } else {
+              game->printMessage("Invalid response, trade cancelled.");
+              continue;
+            }
           } catch (std::out_of_range) {
-          
+            game->printMessage("Invalid amount of money to receive.");
+            continue;
           }
         } catch (std::out_of_range) {
-          
+          game->printMessage("Invalid amount of money to give.");
+          continue;
         }
       }
+    } else if (s == "improve") {
+      std::string buildingName;
+      std::string op;
+      in >> buildingName;
+      in >> op;
+      if (in.fail()) {
+        game->printMessage("Not enough arguments.");
+        break;
+      }
+      if (op == "sell") {
+        try{
+          //sellImprovement(buildingName);
+        } catch(/*to be added*/) {
+          game->printMessage("Cannot sell.");
+          continue;
+        }
+      } else if (op == "buy") {
+        try{
+          //buyImprovement(buildingName);
+        } catch (/*to be added*/) {
+          game->printMessage("Cannot buy.");
+          continue;
+        }
+      } else {
+        game->printMessage("Invalid operation.");
+      }
+    } else if (s == "mortgage") {
+      std::string buildingName;
+      in >> buildingName;
+      if (in.fail()) {
+        game->printMessage("Not enough arguments.");
+        break;
+      }
+      try {
+        //mortgage(buildingName);
+      } catch (/*to be added*/) {
+        game->printMessage("Cannot mortgage.");
+      }
+    } else if (s == "unmortgage") {
+      std::string buildingName;
+      in >> buildingName;
+      if (in.fail()) {
+        game->printMessage("Not enough arguments.");
+        break;
+      }
+      try {
+        //unmortgage(buildingName);
+      } catch (/*to be added*/) {
+        game->printMessage("Cannot unmortgage.");
+      }
+    } else if (s == "bankrupt") {
+      //to be added;
+    } else if (s == "assets") {
+      //game->printAssets();
+    } else if (s == "all") {
+      //game->printAllAssets();
+    } else if (s == "save") {
+      std::string fileName;
+      std::ofstream realFile {fileName};
+      game->saveGame(realFile);
+    } else if (s == "yes") {
+      //game->purchase();
+    } else if (s == "no") {
+      game->printMessage("Not purchased.");
+      game->auction();
     }
   }
 }
+
