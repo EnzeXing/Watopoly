db77ff72013dcac4dc5e48a671a5199478cef35c
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-12 12:45:48 -0400

    Update Building.h

--- Building.h
+++ Building.h
@@ -5,16 +5,22 @@
 #include <algorithm>
 #include <iostream>
 
-class Player;
+class Player; // forward declaration
 
+// abstract class of buildings
 class Building {
+  // basic information of the building
   private:
     std::string name;
     int position;
   public:
+  // constructor
     Building(std::string name, int position);
+  // accessor
     std::string getName();
+  // accessor
     int getPosition();
+  // virtual method for visitor pattern
     virtual void accept(Player & p) = 0;
 };
 
