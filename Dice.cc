940500316c6697faa26317882cbe3b0c7120a242
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-12 12:49:15 -0400

    Update Dice.cc

--- Dice.cc
+++ Dice.cc
@@ -2,6 +2,7 @@
 
 
 unsigned int Dice::roll() {
+        // random number generation algorithm
         std::random_device r;
         std::default_random_engine e1(r());
         std::uniform_int_distribution<int> uniform_dist(1, 6);
