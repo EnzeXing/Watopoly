3801ea56090004e79de7c28bf9eca75ad8e51189
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-12 13:41:05 -0400

    Update commandInput.h

--- commandInput.h
+++ commandInput.h
@@ -4,14 +4,22 @@
 #include "Game.h"
 #include <iostream>
 
+// controller of the MVC model
 class CommandInput {
+    // shared pointer to the Game class
     std::shared_ptr<Game> game;
   public:
+    // constructor
     CommandInput(std::shared_ptr<Game> game);
+    // takes in commandline input and process it
     void readInput(std::istream & in);
+    // handles the sitution of building purchase
     void purchaseOrNot(std::istream & in, std::string building);
+    // handles the sitution of aution
     void auction(std::istream & in, std::string building);
+    // handles the sitution when the player does not have enough money
     void notEnoughMoney(std::istream & in, int amount);
+    // handles the sitution when the player is at Tim Hortons
     void TimHortons(std::istream & in);
 };
 
