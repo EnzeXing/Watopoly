21a4668fef9d46252b38a1d2b81959af59a22bcf
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-13 14:55:01 -0400

    Update test2.cc

--- test2.cc
+++ test2.cc
@@ -1,1 +1,34 @@
+#include "commandInput.h"
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <fstream>
+
+int main(int argc, std::string argv[]) {
+    if (argc == 1) {
+      std::map<std::string, std::string> players;
+      players["Andrew"] = "A";
+      players["Ivan"] = "I";
+      players["Fatday"] = "F";
+      players["Klaus"] = "K";
+      players["Lingwei"] = "L";
+      players["Steven"] = "S";
+    
+    // std::ifstream infile{"Game1.txt"};
+    
+      CommandInput input{std::make_shared<Game>(players)};
+      input.readInput(std::cin);
+      return 0;
+    } else if (argc == 3) {
+      std::ifstream infile{argv[2]};
+      CommandInput input{std::make_shared<Game>(infile)};
+      input.readInput(std::cin);
+      return 0;
+    } else {
+      //testing mode
+      return 0;
+    }
+}
+      
 
