c61593cc945df65e8a05ae12c64a5e5f1bc8625f
Ivan-Yifan-Hu <61921653+Ivan-Yifan-Hu@users.noreply.github.com>
2020-08-14 15:40:31 -0400

    Update Exception.cc

--- Exception.cc
+++ Exception.cc
@@ -32,4 +32,4 @@ MortgageException::MortgageException(std::string m) : WatopolyException(m) {}
 
 hasWon::hasWon(std::string m) : WatopolyException(m) {}
 
-Bankrupt::Exception(std::string m) : WatopolyException(m) {}
+BankruptException::BankruptException(std::string m) : WatopolyException(m) {}
